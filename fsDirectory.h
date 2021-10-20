dffbf2dc2f0ef3bcd588af906cb1c8759aa16d2d
josephkois <josephkois@gmail.com>
2021-10-20 10:32:52 -0700

    added header files

---
+++ fsDirectory.h
@@ -0,0 +1,14 @@
+/**************************************************************
+* Class:  CSC-415-0# Fall 2021
+* Names: 
+* Student IDs:
+* GitHub Name:
+* Group Name:
+* Project: Basic File System
+*
+* File: fsDirectory.h
+*
+* Description: holds the directory functions like create 
+* directory for now, will use in conjunction with mfs.h later
+* on
+**************************************************************/
