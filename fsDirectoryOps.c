7f8a1f8328524b20ad90aad0d39ff66f3af02201
Joseph Kois <joe@Josephs-MacBook-Pro.local>
2021-11-02 19:19:34 -0700

    added 4 files

---
+++ fsDirectoryOps.c
@@ -0,0 +1,13 @@
+/**************************************************************
+* Class:  CSC-415-0# Fall 2021
+* Names: 
+* Student IDs:
+* GitHub Name:
+* Group Name:
+* Project: Basic File System
+*
+* File: fsDirectoryOps.c
+*
+* Description: Represents entries whether they be files
+or directories.
+**************************************************************/
